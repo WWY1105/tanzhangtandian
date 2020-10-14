0edc4b8dd72a948fdc3a909b78a1b883a917d854
weiweiying <weiying.wei@wecinema.club>
2020-10-14 18:30:45 +0800

    a

--- app.js
+++ app.js
@@ -245,6 +245,82 @@ App({
     })
 
   },
+  convertHtmlToText: function convertHtmlToText(inputText) {
+    var returnText = "" + inputText;
+    returnText = returnText.replace(/<\/div>/ig, '\r\n');
+    returnText = returnText.replace(/<\/li>/ig, '\r\n');
+    returnText = returnText.replace(/<li>/ig, '  *  ');
+    returnText = returnText.replace(/<\/ul>/ig, '\r\n');
+    //-- remove BR tags and replace them with line break
+    returnText = returnText.replace(/<br\s*[\/]?>/gi, "\r\n");
+
+    //-- remove P and A tags but preserve what's inside of them
+    returnText = returnText.replace(/<p.*?>/gi, "\r\n");
+    returnText = returnText.replace(/<a.*href="(.*?)".*>(.*?)<\/a>/gi, " $2 ($1)");
+
+    //-- remove all inside SCRIPT and STYLE tags
+    returnText = returnText.replace(/<script.*>[\w\W]{1,}(.*?)[\w\W]{1,}<\/script>/gi, "");
+    returnText = returnText.replace(/<style.*>[\w\W]{1,}(.*?)[\w\W]{1,}<\/style>/gi, "");
+    //-- remove all else
+    returnText = returnText.replace(/<(?:.|\s)*?>/g, "");
+
+    //-- get rid of more than 2 multiple line breaks:
+    returnText = returnText.replace(/(?:(?:\r\n|\r|\n)\s*){2,}/gim, "\r\n\r\n");
+
+    //-- get rid of more than 2 spaces:
+    returnText = returnText.replace(/ +(?= )/g, '');
+
+    //-- get rid of html-encoded characters:
+    returnText = returnText.replace(/ /gi, " ");
+    returnText = returnText.replace(/&/gi, "&");
+    returnText = returnText.replace(/"/gi, '"');
+    returnText = returnText.replace(/</gi, '<');
+    returnText = returnText.replace(/>/gi, '>');
+
+    return returnText;
+  },
+  _wxPay: function (payData, callback) {
+    let _that = this
+    wx.requestPayment({
+      timeStamp: payData.timeStamp,
+      nonceStr: payData.nonceStr,
+      package: payData.packages,
+      signType: payData.signType,
+      paySign: payData.paySign,
+      success: function (result) {
+        if (result.errMsg == 'requestPayment:ok') {
+          wx.showLoading({
+            title: '支付中'
+          })
+          let url = '/home/payment/queryOrder';
+          let data = {
+            orderId: payData.orderId,
+          }
+          _that.request('get', url, data, (res) => {
+            if (callback) callback(res)
+          })
+        } else {
+          wx.showToast({
+            title: '支付异常' + result.errMsg,
+            icon: 'none',
+            mask: true
+          });
+        }
+      },
+      fail: function (e) {
+        // 取消支付
+        console.info(e)
+        if (e == 'requestPayment:fail cancel') {
+          wx.showToast({
+            title: '支付取消',
+            icon: 'none',
+            mask: true
+          });
+        }
+      },
+    })
+
+  },
   getLocation: function (callback) { //获取用户定位
     // wx.showLoading({
     //   title: '加载中',
@@ -296,13 +372,13 @@ App({
     scene: '',
     location: {},
      //  测试
-    //  ajaxOrigin: "https://saler.sharejoy.cn",
-    //  urlOrigin: "https://saler.sharejoy.cn", 
+     ajaxOrigin: "https://saler.sharejoy.cn",
+     urlOrigin: "https://saler.sharejoy.cn", 
 
 
     //  正式
-    ajaxOrigin: "https://saler.ishangbin.com",
-    urlOrigin: "https://saler.ishangbin.com"
+    // ajaxOrigin: "https://saler.ishangbin.com",
+    // urlOrigin: "https://saler.ishangbin.com"
   },
   util: util
 })
